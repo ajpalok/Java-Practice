72a19537e1dd76e6ff60fd53508cfe021201c62e
Abrar Jahin <palok1969@gmail.com>
2023-07-18 20:39:47 +0600

    Question wise Edit

--- 07 Advance Calculator Mid Question/AdvanceCalculator.java
+++ 07 Advance Calculator Mid Question/AdvanceCalculator.java
@@ -1,15 +1,15 @@
 public class AdvanceCalculator  extends Calculator{
-    public int[] arr;
+    public int[] number;
     public int c;
 
     public int sum(){
-        for(int i:arr) c+=i;
+        for(int i:number) c+=i;
         return super.sum()+c;
     }
 
     public AdvanceCalculator(int a, int b, int[] arr) {
         super(a, b);
-        this.arr = arr;
+        this.number = arr;
     }
     
 }
